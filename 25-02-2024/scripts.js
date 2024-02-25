9969777361004e3fb7ae332a4a91d39e217d3692
ugur <ugur.fastcoon@gmail.com>
2024-02-25 14:07:22 +0300

    Update index.html

---
+++ 25-02-2024/scripts.js
@@ -0,0 +1,32 @@
+const arrayVar = [
+    13,
+    25,
+    4,
+    "Hello World!",
+    true,
+    -5,43,
+    [21, 39, "Hello World"],
+    "Javascript",
+
+
+];
+
+console.log[arrayVar];
+
+
+
+
+
+
+
+
+
+
+
+
+const IndexOfElement = arrayVar.indexOf("Javascript")
+
+
+
+
+
