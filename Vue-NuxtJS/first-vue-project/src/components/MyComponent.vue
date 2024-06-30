00cdceca6267398fdd7c8eeaad7dee1c6bbada2a
canberkberen <142876502+canberkberen@users.noreply.github.com>
2024-06-30 16:45:11 +0300

    first vue project

---
+++ Vue-NuxtJS/first-vue-project/src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  exampleProp: String
+})
+
+let exampleVariable = 2024
+</script>
+
+<template>
+  <div>
+    <h1>This is my component</h1>
+  </div>
+  <div>
+    <h2>It is a component</h2>
+    {{ exampleProp }} - {{ exampleVariable }}
+  </div>
+</template>
---
+++ Vue-NuxtJS/first-vue-project/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <MyComponent exampleProp="This is an example prop" />
+  </div>
+</template>
+
+<script setup>
+import MyComponent from '../components/MyComponent.vue'
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
