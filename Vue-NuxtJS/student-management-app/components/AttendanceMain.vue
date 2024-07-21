3c0497cc10cf9c4b0405965a7d4d85d8a125c8e4
canberkberen <142876502+canberkberen@users.noreply.github.com>
2024-07-21 11:44:51 +0300

    done

--- Vue-NuxtJS/student-management-app/components/AttendanceMain.vue
+++ Vue-NuxtJS/student-management-app/components/AttendanceMain.vue
@@ -26,6 +26,64 @@
       >
     </div>
   </div>
+  <div>
+    <div class="my-10 w-full">
+      <table class="min-w-full text-left">
+        <thead class="border bg-white">
+          <tr class="w-full">
+            <th class="p-5">Yoklama Durumu</th>
+            <th class="p-5">Öğrenci Adı</th>
+            <th class="p-5">Öğrenci Soyadı</th>
+            <th class="p-5">Öğrenci Numarası</th>
+            <th class="p-5">Öğrenci Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            class="border-b bg-gray-700 text-white"
+            v-for="student in courses.find(
+              (course) => course._id === selectedCourse
+            )?.students"
+          >
+            <td class="p-5">
+              <input
+                type="checkbox"
+                :checked="existingStudents?.includes(student._id)"
+                @change="
+                  existingStudents.includes(student._id)
+                    ? (existingStudents = existingStudents?.filter(
+                        (id) => id !== student._id
+                      ))
+                    : existingStudents.push(student._id)
+                "
+              />
+              <span class="mx-3">Mevcut</span>
+            </td>
+            <td class="p-5">{{ student.name }}</td>
+            <td class="p-5">{{ student.lastName }}</td>
+            <td class="p-5">{{ student.phoneNumber }}</td>
+            <td class="p-5">{{ student.mail }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="flex w-full justify-end mt-5">
+        <v-btn color="primary" @click="handleCreateAttendance"
+          >Yoklamayı Kaydet</v-btn
+        >
+      </div>
+    </div>
+  </div>
+
+  <v-snackbar v-model="snackbar" location="top">
+    {{ text }}
+
+    <template v-slot:actions>
+      <v-btn color="pink" variant="text" @click="snackbar = false">
+        Kapat
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script setup>
@@ -36,12 +94,40 @@ import "@vuepic/vue-datepicker/dist/main.css";
 import moment from "moment";
 
 const courses = ref([""]);
-const attendanceDateDialog = ref(false);
+const existingStudents = ref([]);
+const snackbar = ref(false);
+const text = ref("");
 
 const newAttendanceForm = ref({
   classId: "",
   attendanceDate: "",
 });
+
+const selectedCourse = ref("");
+
+const handleCreateAttendance = async () => {
+  try {
+    const newAttendanceResponse = await axios.post(
+      "http://localhost:3001/attendance/record",
+      {
+        attendanceDate: moment(newAttendanceForm.value.attendanceDate).format(
+          "YYYY-MM-DD"
+        ),
+        classId: selectedCourse.value,
+        students: existingStudents.value,
+      }
+    );
+
+    if (newAttendanceResponse.status === 200) {
+      snackbar.value = true;
+      text.value = "Yoklama başarıyla kaydedildi.";
+      return;
+    }
+  } catch (e) {
+    snackbar.value = true;
+    text.value = "Yoklama kaydedilirken bir hata oluştu.";
+  }
+};
 
 const handleGetAttendance = async () => {
   try {
@@ -55,9 +141,18 @@ const handleGetAttendance = async () => {
       }
     );
 
-    console.log(attendanceResponse.data);
+    if (attendanceResponse.status === 200) {
+      selectedCourse.value = newAttendanceForm.value.classId;
+
+      existingStudents.value = attendanceResponse?.data?.[0]?.students;
+      snackbar.value = true;
+      text.value = "Yoklama başarıyla getirildi.";
+      return;
+    }
   } catch (e) {
-    console.log(e);
+    snackbar.value = true;
+    text.value = "Yoklama getirilirken bir hata oluştu.";
+    return;
   }
 };
 
